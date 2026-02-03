4f42032c9179c74e71bb39a9d12481e0ee5d27bd
manunaik128 <manumn2005@gmail.com>
2026-02-03 21:17:07 +0530

    updated

---
+++ L1 variables datatypes/script.js
@@ -0,0 +1,44 @@
+fullName ="Manu naik";
+fullname ="Manu M naik";
+Y=null;
+y=undefined;
+radius=11.5;
+isAdmin=true;
+isFollow=true;
+console.log(isFollow)
+
+console.log(fullname);
+console.log(fullName);
+
+// key words
+// var: variable can be used re-declared & updated.A global scope variable. (older not uses)
+// let: variable can be updated but cannot be re-declared. A block scope variable.
+// const: variable cannot be updated or re-declared. A block scope variable. (once assigned value cannot be changed)
+
+let name = "manu";
+let price = 200;
+let age=20;
+console.log(name);
+console.log(price);
+console.log(age);
+
+const name1="naik";
+console.log(name1);
+
+let a;
+console.log(a);
+
+//blocks using flower braces {}
+{
+    let a=5;
+    console.log(a);
+}
+
+{
+    let a=10;
+    console.log(a);
+}
+
+//Data types in javascript
+
+ 
