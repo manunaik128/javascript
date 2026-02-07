a8e14e6a472603cb054eef7739344baad1a817a5
manunaik128 <manumn2005@gmail.com>
2026-02-07 10:38:56 +0530

    commited

---
+++ L2 operators and conditional statements/script2.js
@@ -0,0 +1,29 @@
+// comments elements 1. single-line comments 2. multiline comments
+
+ // 1. this is a single-line comment
+
+/* 2. this is a 
+multiline comment */
+
+
+//Operators
+
+// Arithmetic Operators: +, -, *, /, %, ++, --
+
+let a =10;
+let b =20;
+console.log("A+B:",a+b);
+console.log("A-B:",a-b);
+console.log("A*B: ",a*b);
+console.log("A/B:",a/b);
+console.log("A%B:",a%b);
+
+//Unary Operators: ++, --
+console.log("Increment A:",++a);  //++a means first increment then use // a++ means first use then increment
+console.log("Decrement B:",--b); //--b means first decrement then use // b-- means first use then decrement
+
+
+
+
+
+
