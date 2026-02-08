a01f3532bb5046294763c50be69e32c563af7c12
manunaik128 <manumn2005@gmail.com>
2026-02-08 20:51:29 +0530

    opeartors

--- L2 operators and conditional statements/script2.js
+++ L2 operators and conditional statements/script2.js
@@ -3,8 +3,7 @@
  // 1. this is a single-line comment
 
 /* 2. this is a 
-multiline comment */
-
+multiline comment */  
 
 //Operators
 
@@ -22,8 +21,27 @@ console.log("A%B:",a%b);
 console.log("Increment A:",++a);  //++a means first increment then use // a++ means first use then increment
 console.log("Decrement B:",--b); //--b means first decrement then use // b-- means first use then decrement
 
+//Assignment Opertors (=.+=,-=,*=,%=,**=)
 
+let x=10;
+let y=5;
+y += 4;
+x -= 2;
+y *= 4;
+console.log(y);
+console.log(x);
 
+//comparision Operators (equal to ==, not equal to !=, equal to & type of data also checks ===, not equal to & type!==) >,>=,<,<=
 
+let num1 =40;
+let num2 =30;
+
+console.log("40==30",num1==num2);
+console.log("40 >30",num1>num2);
+console.log("40!=30",num1 != num2);
+
+let x2 =10;
+let x1 ="10";
+console.log("x1===x2",x1===x2) //here === checks for also a data type in this case string and number compares so its return false. its not possible in == case
 
 
