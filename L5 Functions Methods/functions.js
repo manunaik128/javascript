b2c7fc0b457e109ce4aa92c2f9469b8da1009687
manunaik128 <manumn2005@gmail.com>
2026-03-01 13:40:40 +0530

    functions completed

--- L5 Functions Methods/functions.js
+++ L5 Functions Methods/functions.js
@@ -77,15 +77,104 @@ return count;
 //callbackFunction:Here,it is a function to execute for each element in the array
 //defination="A callback is a function passed as an argument to another function"
 
-/*function abc(){
+function abc(){
     console.log("hello")
 }
 function myfunc(abc){
     return abc;
-}*/
+}
 
-let arr =[1,2,3,4,5];
+let arr =["karnataka","delhi","goa"]; //for each is used only for array not for strings
 
-arr.forEach(function printVal(val){ //value at each index
-console.log(val );
-})
+arr.forEach((val,idx,arr) =>{ //value at each index
+    console.log(val.toUpperCase(),idx,arr); //higher order function / method= they can return another function a the output 
+});
+
+
+//Practice qst 
+//for given array of number,print the square of each value using forEach loop
+
+let nums =[2,3,4,5,6];
+
+let calcSquare =(num) =>{
+    console.log(num *num ); //num **2
+};
+
+nums.forEach(calcSquare);
+
+
+//Map = creates a new array with the result of some operation.The value its callback returns are used to form new array
+
+let numbers =[2,3,4,5,6];
+
+let newArr = numbers.map((val)=>{  //use to print number 
+    return val * val; //here it will print square of the numbers
+    console.log(val);
+});
+console.log(newArr);
+
+let numSquare =(num) =>{
+    console.log(numbers *numbers ); //num **2
+};
+
+
+
+//Filter method = creates a new array of elements thet give true for  condition/filter.
+let arr1 = [1,2,3,4,5,6,7];
+
+let evenArr=arr1.filter((val)=>{
+    return val % 2 ===0;
+});
+
+console.log(evenArr);
+
+
+//Reduce method = performs some operations and reduce the array to single value.It returns that single value
+//sum calculate
+let arr2 = [5,6,5,1,3];
+
+const output=arr2.reduce((prev,curr)=>{   //prev=previous element  curr=current
+   return prev > curr ? prev :curr //getting larger element from the array
+}); 
+
+console.log(output); 
+
+
+// practice qst2
+/* take input from user.crate an array of numbers from 1 to n.
+use the reduce method to calculate sum of all numbers in the array.
+use the reduce method to calculate product of all numbers in the array.*/
+
+let marks =[97,64,99,95,65,82];
+
+let toppers=marks.filter((val) =>{
+    return val >90;
+});
+
+console.log(toppers);
+
+
+/* take input from user.crate an array of numbers from 1 to n.
+use the reduce method to calculate sum of all numbers in the array.
+use the reduce method to calculate product of all numbers in the array.*/
+
+let n=prompt("Enter a number");
+
+let array =[];
+
+for(let i=1; i<=n; i++){
+    array[i-1]=i; //storing i value in n array
+}
+console.log(array);
+
+let total= array.reduce((res,curr)=>{
+    return res +curr;
+});
+
+console.log("total=",total);
+
+let factorial= array.reduce((res,curr)=>{
+    return res * curr;
+});
+
+console.log("factorial=",factorial);
